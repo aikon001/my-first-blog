160ff6db08d45cdcc14533545112c8e5ce3458c4
aikon001 <46329166+aikon001@users.noreply.github.com>
2019-01-02 21:05:46 +0100

    Update settings.py

--- testsite/settings.py
+++ testsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'og@vh5-ohj)qbkag%+cg$wkg2y+@%b&pq)vfh1rg*@e((fkjlp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
