073e0e4b7880ab92ad771260ae9ba5437abbe846
aikon001 <46329166+aikon001@users.noreply.github.com>
2019-01-18 16:02:58 +0100

    Add files via upload

--- testsite/settings.py
+++ testsite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import django_heroku 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'og@vh5-ohj)qbkag%+cg$wkg2y+@%b&pq)vfh1rg*@e((fkjlp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['demotestsite.herokuapp.com']
 
@@ -144,4 +144,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'testsite/static'),]
 # https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
 
